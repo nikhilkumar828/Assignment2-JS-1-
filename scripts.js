d444ad1570183e029c3a2e5d38b45826e344d7dd
Nikhil Vallore <Nikhil_Vallore@epam.com>
2019-07-10 16:48:44 +0530

    intialCommit

---
+++ scripts.js
@@ -0,0 +1,305 @@
+var data = [
+    {
+        imageSrc: "https://dbjk47w5ep2rm.cloudfront.net/blog/wp-content/uploads/2015/03/Screen-Shot-2015-03-19-at-12.16.37-PM.png",
+        postTitle: "Title One",
+        postDate: "29th June , 2019",
+        postCategory: "BCC",
+        postDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        imageSrc: "https://dbjk47w5ep2rm.cloudfront.net/blog/wp-content/uploads/2015/03/Screen-Shot-2015-03-19-at-12.16.37-PM.png",
+        postTitle: "Title Two",
+        postDate: "22th June , 2019",
+        postCategory: "CNN",
+        postDesc: "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        imageSrc: "https://dbjk47w5ep2rm.cloudfront.net/blog/wp-content/uploads/2015/03/Screen-Shot-2015-03-19-at-12.16.37-PM.png",
+        postTitle: "Title Three",
+        postDate: "20th June , 2019",
+        postCategory: "IndiaToday",
+        postDesc: "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        imageSrc: "https://dbjk47w5ep2rm.cloudfront.net/blog/wp-content/uploads/2015/03/Screen-Shot-2015-03-19-at-12.16.37-PM.png",
+        postTitle: "Title Four",
+        postDate: "20th June , 2019",
+        postCategory: "FoxNews",
+        postDesc: "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        imageSrc: "https://dbjk47w5ep2rm.cloudfront.net/blog/wp-content/uploads/2015/03/Screen-Shot-2015-03-19-at-12.16.37-PM.png",
+        postTitle: "Title One",
+        postDate: "29th June , 2019",
+        postCategory: "TV9",
+        postDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        imageSrc: "https://dbjk47w5ep2rm.cloudfront.net/blog/wp-content/uploads/2015/03/Screen-Shot-2015-03-19-at-12.16.37-PM.png",
+        postTitle: "Title Two",
+        postDate: "22th June , 2019",
+        postCategory: "Republic",
+        postDesc: "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        imageSrc: "https://dbjk47w5ep2rm.cloudfront.net/blog/wp-content/uploads/2015/03/Screen-Shot-2015-03-19-at-12.16.37-PM.png",
+        postTitle: "Title Three",
+        postDate: "20th June , 2019",
+        postCategory: "IndiaToday",
+        postDesc: "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        imageSrc: "https://dbjk47w5ep2rm.cloudfront.net/blog/wp-content/uploads/2015/03/Screen-Shot-2015-03-19-at-12.16.37-PM.png",
+        postTitle: "Title Four",
+        postDate: "20th June , 2019",
+        postCategory: "CNN",
+        postDesc: "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        imageSrc: "https://dbjk47w5ep2rm.cloudfront.net/blog/wp-content/uploads/2015/03/Screen-Shot-2015-03-19-at-12.16.37-PM.png",
+        postTitle: "Title Three",
+        postDate: "20th June , 2019",
+        postCategory: "TV9",
+        postDesc: "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        imageSrc: "https://dbjk47w5ep2rm.cloudfront.net/blog/wp-content/uploads/2015/03/Screen-Shot-2015-03-19-at-12.16.37-PM.png",
+        postTitle: "Title Four",
+        postDate: "20th June , 2019",
+        postCategory: "FoxNews",
+        postDesc: "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum."
+    },
+];
+
+var categories=["All","BCC","IndiaToday","Republic","TV9","CNN","FoxNews"];
+
+function setData() {
+    document.getElementById("postImageId").src = data[0].imageSrc;
+    document.getElementById("postTitleId").innerHTML = data[0].postTitle;
+     document.getElementById("postTitleDescId").innerHTML = data[0].postCategory;
+    document.getElementById("postDescId").innerHTML = data[0].postDesc;
+    for (var entries = 1; entries < data.length; entries++) {
+        var elmnt = document.getElementById("post1");
+        var clone = elmnt.cloneNode(true);
+        clone.id = "post" + (entries + 1);
+        document.getElementById("content").appendChild(clone);
+        var cloneChild = document.getElementById("post" + (entries + 1)).childNodes;
+
+        cloneChild[0].id = "postImageId" + (entries + 1);
+        var postChilds = cloneChild[1].childNodes;
+        for (var postIdChanges = 0; postIdChanges < postChilds.length; postIdChanges++) {
+            postChilds[postIdChanges].id = postChilds[postIdChanges].id + (entries + 1);
+        }
+
+        document.getElementById("postImageId" + (entries + 1)).src = data[entries].imageSrc;
+        document.getElementById("postTitleId" + (entries + 1)).innerHTML = data[entries].postTitle;
+         document.getElementById("postTitleDescId"+(entries+1)).innerHTML = data[entries].postCategory;
+        document.getElementById("postDescId" + (entries + 1)).innerHTML = data[entries].postDesc;
+
+    }
+}
+
+
+function addHeader(){
+    //Header
+    var div = document.createElement('div');
+    // div.name = "mainContainer";
+    div.className = "titleMain";
+    div.id="titleContainer";
+    document.getElementById("headerTitle").appendChild(div);
+
+    div = document.createElement('h2');
+    div.className = "titleHeading";
+    div.innerHTML="NEWSFEED";
+    document.getElementById("titleContainer").appendChild(div);
+
+    div = document.createElement('h6');
+    div.className = "titleDesc";
+    div.innerHTML="<i>Yet another newsfeed</i>";
+    document.getElementById("titleContainer").appendChild(div);
+}
+
+function addFooter(){
+//Footer
+
+var div = document.createElement('div');
+div.innerHTML="<br>"
+document.getElementById("wrapper").appendChild(div);
+
+document.getElementById("footerTitle").className="footerContent";
+var div = document.createElement('div');
+div.className = "marginForFooterContent";
+div.innerHTML = "&copy; NewsFeed 2019";
+document.getElementById("footerTitle").appendChild(div);
+}
+
+function addMain(){
+//Body
+var div = document.createElement('div');
+div.id = "mainContainer"
+div.className="flxRow";
+document.getElementById("wrapper").appendChild(div);
+
+div = document.createElement('div');
+div.id = "content"
+div.className = "flxColumn allPosts";
+document.getElementById("mainContainer").appendChild(div);
+
+div = document.createElement('div');
+div.className = "sidePanelStyle";
+div.id = "sidePanel"
+document.getElementById("mainContainer").appendChild(div);
+
+div = document.createElement('div');
+div.id = "post1"
+div.className = "flxRow allPosts";
+document.getElementById("content").appendChild(div);
+
+div = document.createElement('img');
+div.id = "postImageId"
+div.className = "postImage";
+document.getElementById("post1").appendChild(div);
+
+div = document.createElement('div');
+div.id = "postContent"
+div.className = "marginForPost";
+document.getElementById("post1").appendChild(div);
+////
+div = document.createElement('h2');
+div.id = "postTitleId"
+document.getElementById("postContent").appendChild(div);
+
+div = document.createElement('h6');
+div.id = "postTitleDescId"
+div.className = "spacingWithInPosts";
+document.getElementById("postContent").appendChild(div);
+
+div = document.createElement('p');
+div.id = "postDescId"
+div.className = "spacingWithInPosts";
+document.getElementById("postContent").appendChild(div);
+
+div = document.createElement('button');
+div.id = "postContinueBtnId"
+div.innerHTML = "Continue Reading";
+div.className = "postButton spacingWithInPosts";
+document.getElementById("postContent").appendChild(div);
+addSidePanel();
+
+
+}
+
+function addSidePanel(){
+
+    //Category
+    div = document.createElement('h3');
+    div.innerHTML = "Select Category";
+    document.getElementById("sidePanel").appendChild(div);
+
+    //ListBox Creation
+    div = document.createElement('select');
+    div.id = "categoryLstBox";
+    div.className = "lstBox";
+    div.addEventListener("change",listBoxSelection);
+    document.getElementById("sidePanel").appendChild(div);
+
+    //categories
+
+    for(var allCategories=0;allCategories<categories.length;allCategories++){
+    // Create an Option        
+    var opt = document.createElement("option");
+    // Add an Option object to List Box
+    document.getElementById("categoryLstBox").options.add(opt);
+    opt.text = categories[allCategories];
+    opt.value = allCategories;
+    document.getElementById("categoryLstBox").options.add(opt);
+    }
+
+    div = document.createElement('div');
+    div.innerHTML="<br><br>";
+    document.getElementById("sidePanel").appendChild(div);
+
+    //Subscribe
+    div = document.createElement('h3');
+    div.innerHTML = "Subscribe<br><br>";
+    document.getElementById("sidePanel").appendChild(div);
+
+    //Email 
+    div = document.createElement('form');
+    div.id = "emailIdAction";
+    document.getElementById("sidePanel").appendChild(div);
+    document.getElementById("emailIdAction").action = "#";
+
+    div = document.createElement('h5');
+    div.innerHTML = "Email Id:<br>";
+    document.getElementById("sidePanel").appendChild(div);
+
+    div = document.createElement('input');
+    div.id = "emailIdTextBox";
+    div.className="txtBoxMeasures";
+    div.placeHoler="EmailAddress";
+    div.type = "text";
+    document.getElementById("sidePanel").appendChild(div);
+
+    //Submit Email
+    div = document.createElement('input');
+    div.id = "emailIdSubmitBtn";
+    div.className="btnSubscribe";
+    div.value="Subscribe";
+    div.type = "submit";
+    div.addEventListener("click",submitEmail);
+    document.getElementById("sidePanel").appendChild(div);
+
+
+
+}
+
+function create() {
+    
+    addHeader();
+
+
+    addMain();
+
+    addFooter();
+
+    setData();
+}
+
+function listBoxSelection(){
+ 
+    var slectedValue=0;
+    
+    var listBox = document.getElementById("categoryLstBox");
+    selectedValue=listBox.value;
+    console.log(selectedValue);
+    var posts= document.getElementById("content").childNodes;
+    for(var eachPost=0;eachPost<posts.length;eachPost++){
+        if(selectedValue!=0){
+        if(posts[eachPost].childNodes[1].childNodes[1].textContent!=categories[selectedValue]){
+            document.getElementById("post"+(eachPost+1)).style.display = "none";
+        }
+        else{
+            document.getElementById("post"+(eachPost+1)).style.display = "flex";
+        }
+    }
+    else{
+        document.getElementById("post"+(eachPost+1)).style.display = "flex";
+    }
+    }
+}
+
+
+function submitEmail(){
+    let mailId = document.getElementById("emailIdTextBox").value;
+    var re = /\S+@\S+\.\S+/;
+    if(re.test(mailId)){
+        localStorage.setItem('mailId', JSON.stringify(mailId));
+        document.getElementById("emailIdTextBox").value="";
+
+    }
+    else{
+        alert("Enter Valid Email address");
+    }
+    
+}
